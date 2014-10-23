5d2502a3460adbb3029b8398c04b9fd9e79f8067
graysonthemason <gray.reinhard@gmail.com>
2014-10-23 16:04:33 -0400

    redraw rate

--- pong.js
+++ pong.js
@@ -4,18 +4,22 @@ $(function(){
   var mouse;
   var x  = 0;
   var y  = 0;
-  var dx = 1;
-  var dy = 1;
+  var dx = 3;
+  var dy = 3;
   var paddleSize = 160;
+  var g = 255;
+  var b = 255;
 
-  game = document.getElementById('game');
+  var game = document.getElementById('game');
 
   init();
 
   function init() {
     context = game.getContext('2d');
-    setInterval(drawBall, 1);
-  }
+    // setInterval(drawBlocks, 1);
+    setInterval(drawBall, 9);
+  };
+
 
   function drawBall() {
     if (y > 640) {
@@ -25,18 +29,24 @@ $(function(){
       dx = 1;
       dy = 1;
     };
+    if (y > 90 && y < 140 && x > 300 && x < 800) {
+      dy =- dy;
+      g -= 30;
+      b -= 30;
+    }
     if (x < 0 || x > 960) {
       dx =- dx };
     if (y < 0 || y > 800) {
       dy =- dy };
     if (y > 595 && x <= mouse + 80 && x >= mouse - 80) {
       dy =- dy;
-      dy = dy*1.1;
-      dx = dx*1.1;
+      dy = dy*1.05;
+      dx = dx*1.05;
       paddleSize -= 2;
     };
     context.clearRect(0, 0, 960, 700);
     drawPaddle(mouse);
+    drawBlocks();
     context.beginPath();
     context.fillStyle = "rgb(255,255,255)";
     context.arc(x, y, 8, 0, Math.PI*2, true);
@@ -45,14 +55,22 @@ $(function(){
     y += dy;
   };
 
+
   function drawPaddle(x) {
-    console.log(x);
-    context.fillRect(x - (paddleSize/2), 600, paddleSize, 10);
     context.fillStyle = "rgb(255,255,255)";
+    context.fillRect(x - (paddleSize/2), 600, paddleSize, 10);
   };
+  function drawBlocks() {
+    context.fillStyle = "rgb(255,"+ g +"," + b +")";
+    // context.fillStyle = "rgb(255,255,255)";
+    context.fillRect(300, 100, 500, 30);
+  };
+
 
   $('canvas').mousemove(function(e) {
     mouse = e.clientX -10;
-    setInterval(drawPaddle(mouse), 1);
+    // setInterval(drawPaddle(mouse), 1);
+    drawPaddle(mouse);
+
   });
 })
