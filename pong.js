2f1ff07af5090cfd08122eceaf5362542fec10de
graysonthemason <gray.reinhard@gmail.com>
2014-10-23 16:31:11 -0400

    working block

--- pong.js
+++ pong.js
@@ -16,28 +16,29 @@ $(function(){
 
   function init() {
     context = game.getContext('2d');
-    // setInterval(drawBlocks, 1);
     setInterval(drawBall, 9);
   };
 
-
   function drawBall() {
     if (y > 640) {
-      alert("game over");
+      alert("GIVE HIM A HOOK");
       x  = 0;
       y  = 0;
       dx = 1;
       dy = 1;
     };
-    if (y > 90 && y < 140 && x > 300 && x < 800) {
-      dy =- dy;
-      g -= 30;
-      b -= 30;
+    if(g>0) {
+      if (y > 90 && y < 140 && x > 240 && x < 740) {
+        dy =- dy;
+        g -= 50;
+        b -= 50;
+      }
     }
     if (x < 0 || x > 960) {
       dx =- dx };
     if (y < 0 || y > 800) {
       dy =- dy };
+
     if (y > 595 && x <= mouse + 80 && x >= mouse - 80) {
       dy =- dy;
       dy = dy*1.05;
@@ -46,7 +47,9 @@ $(function(){
     };
     context.clearRect(0, 0, 960, 700);
     drawPaddle(mouse);
-    drawBlocks();
+    if(g>0) {
+      drawBlocks();
+    }
     context.beginPath();
     context.fillStyle = "rgb(255,255,255)";
     context.arc(x, y, 8, 0, Math.PI*2, true);
@@ -55,22 +58,17 @@ $(function(){
     y += dy;
   };
 
-
   function drawPaddle(x) {
     context.fillStyle = "rgb(255,255,255)";
     context.fillRect(x - (paddleSize/2), 600, paddleSize, 10);
   };
   function drawBlocks() {
     context.fillStyle = "rgb(255,"+ g +"," + b +")";
-    // context.fillStyle = "rgb(255,255,255)";
-    context.fillRect(300, 100, 500, 30);
+    context.fillRect(240, 100, 500, 30);
   };
-
 
   $('canvas').mousemove(function(e) {
     mouse = e.clientX -10;
-    // setInterval(drawPaddle(mouse), 1);
     drawPaddle(mouse);
-
   });
 })
